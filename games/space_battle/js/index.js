5fe212454c04979cb90f212a44f407bc875d9ebb
Riley Chen <riley@Potters-MacBook-Pro.local>
2019-11-12 10:16:21 +0800

    add extended pbject

--- games/space_battle/js/index.js
+++ games/space_battle/js/index.js
@@ -1,15 +1,15 @@
 let preload = new Phaser.Scene('preload');
 let menu = new Phaser.Scene('menu');
 let level1 = new Phaser.Scene('level1');
-let worldX = 800;
-let worldY = 600;
+let worldX = 550;
+let worldY = 700;
 
 
 let config = {
     type: Phaser.AUTO,
     parent: 'game',
-    width: 800,
-    height: 600,
+    width: 550,
+    height: 700,
     pixelArt: true,
     scale: {
     // Ensure the canvas is resized to fit the parent div's dimensions
--- games/space_battle/js/level1.js
+++ games/space_battle/js/level1.js
@@ -1,76 +1,87 @@
 var starfield;
 var player;
 var bullets;
-class EnemyPos{
-    constructor(x, y, tween){
-        this.x = x;
-        this.y = y;
-        this.tween = tween;
-    }
-}
+var enemybullets;
+var lastTime = 0;
+var lastTime2 = 0;
+var cursorKeys;
 
 class Enemy extends Phaser.GameObjects.Sprite{
     constructor(config) {
         super(config.scene, config.x, config.y, 'spaceship1');
         config.scene.physics.world.enable(this);
         config.scene.add.existing(this);
-        this.bullets = level1.physics.add.group({ defaultKey: 'bullet1', maxSize: 10 });
-    }
-
-    fire() {
-        var bullet = this.bullets.get(this.x, this.y);
-        if (bullet) {
-            bullet.setActive(true);
-            bullet.setVisible(true);
-            bullet.body.velocity.y = 250;
-        }
-    }
-
+        //config.scene.physics.body.immovable = true;
+        this.isFire = false;
+        this.lastTime = 0;
+        this.time = 0;
+        
+    } 
 }
 
-var enemies;
-var enemyPos = [];
-
-
-
-function createEnemies(){
-    
-    enemyPos.push(new EnemyPos(100, 100, 300));
-    enemyPos.push(new EnemyPos(700, 100, 500));
-    enemies = level1.physics.add.group();
-    for(var i = 0; i < enemyPos.length; i++){
-        enemy = new Enemy({scene:level1, x:enemyPos[i].x, y:enemyPos[i].y});
-        enemy.body.velocity.y = 20;
-        level1.tweens.add({
-            targets: enemy,
-            duration: 3000,
-            x: enemyPos[i].tween,
-            delay: 1000,
-            ease: 'Sine.easeInOut',
-            repeat: -1,
-            yoyo: true
-        }); 
-        enemies.create(enemy);
-        console.log(enemies.children.entries.length);
+function shoot() {
+       
+    var w = player.body.width;
+    var bullet = bullets.get(player.body.x + w/2, player.body.y);
+    if (bullet) {
+        bullet.setActive(true);
+        bullet.setVisible(true);
+        bullet.body.velocity.y = -400;
     }
-
-    
-
-}
-
-function shoot(event) {
-        if(32==event.keyCode){
-            var w = player.body.width;
-            var bullet = bullets.get(player.body.x + w/2, player.body.y);
-            if (bullet) {
-                bullet.setActive(true);
-                bullet.setVisible(true);
-                bullet.body.velocity.y = -250;
-            }
-        }
+        
         
 }
 
+function fire2(x, y) {
+       
+    var bullet = enemybullets.get(x, y);
+    if (bullet) {
+        bullet.setActive(true);
+        bullet.setVisible(true);
+        bullet.body.velocity.y = 200;
+    }
+        
+        
+}
+
+function fireBullets(){
+    enemies.children.iterate((child) => {
+        var w = child.body.width;
+        var h = child.body.height;
+        fire2(child.body.x + w/2, child.body.y + h/2)
+        
+  })
+   
+}
+
+function createEnemy(x, y){
+    e = new Enemy({scene:level1, x: x, y:y});
+   
+    
+    e.body.setVelocityY(50);
+    e.body.immovable = true;
+    enemy = enemies.create(e);
+    //level1.physics.add.existing(e);
+
+    /*level1.tweens.add({
+           targets: enemy,
+           duration: 3000,
+           x: 300,
+           delay: 200,
+           ease: 'Sine.easeInOut',
+           repeat: -1,
+           yoyo: true,
+       }); */
+    console.log(enemies.children.entries.length);
+
+}
+
+function hitEnemy(bullet, enemy){
+    enemies.remove(enemy, true);
+    bullet.disableBody(false, true); // disableBody( [disableGameObject] [, hideGameObject])
+    console.log('hit')
+    console.log(enemies.children.entries.length);
+}
 
 level1.preload = function ()
 {
@@ -80,11 +91,37 @@ level1.preload = function ()
 
 level1.create = function ()
 {
-    starfield = level1.add.tileSprite(400, 300, 800, 600, 'starfield');
-    player = this.physics.add.sprite(400, 500, 'starship');
+    starfield = level1.add.tileSprite(worldX/2, worldY/2, worldX, worldY, 'starfield');
+    player = this.physics.add.sprite(400, 700, 'starship');
     player.setCollideWorldBounds(true);
     
-    bullets = this.physics.add.group({ defaultKey: 'bullet2', maxSize: 10 });
+    bullets = this.physics.add.group({ defaultKey: 'bullet2', maxSize: 100 });
+    enemybullets = this.physics.add.group({ defaultKey: 'bullet1', maxSize: 100 });
+    enemies = this.physics.add.group({ defaultKey: 'spaceship1', maxSize: 10, runChildUpdate: true });
+
+    e = new Enemy({scene:level1, x: 100, y:100});
+    enemies.add(e);
+    e.body.immovable = true;
+
+    e.body.velocity.y = 20;
+    console.log(e.time);
+
+
+    enemies.children.iterate((child) => {
+    child.update = function (time, delta) {
+        //console.log(child.body);
+        if(this.body.y >= 150 && this.body.y <= 200){
+            
+        }
+        if(this.body.y >= worldY){
+            enemies.remove(child, true);
+            console.log(enemies.children.entries.length);
+        }
+    };
+
+  })
+
+    
    //ship1 = this.physics.add.sprite(400, 100, 'spaceship2');
    //bullet1 = this.physics.add.sprite(400, 300, 'bullet1');
    //bullet2 = this.physics.add.sprite(400, 400, 'bullet2');
@@ -92,17 +129,21 @@ level1.create = function ()
    //ship1.scaleY = 0.35
     
     cursors = level1.input.keyboard.createCursorKeys();
-    
-    this.input.keyboard.on('keydown', shoot, this);
+    cursorKeys = level1.input.keyboard.createCursorKeys();
 
-    createEnemies();
+    //createEnemies();
+
+    this.physics.world.enable(enemies);
+    this.physics.world.enable(bullets);
+    this.physics.add.collider(bullets, enemies, hitEnemy, null, this);
+    this.time.addEvent({ delay: 4000, callback: fireBullets, callbackScope: this, loop: true });
     
     
 },
 
 
-    
-level1.update = function ()
+
+level1.update = function (time, delta)
 {
     
     starfield.tilePositionY -= 1;
@@ -111,11 +152,11 @@ level1.update = function ()
 
     if (cursors.left.isDown)
     {
-        player.setVelocityX(-300);
+        player.setVelocityX(-400);
     }
     else if (cursors.right.isDown)
     {
-        player.setVelocityX(300);
+        player.setVelocityX(400);
     }
 
     if (cursors.up.isDown)
@@ -125,6 +166,13 @@ level1.update = function ()
     else if (cursors.down.isDown)
     {
         player.setVelocityY(300);
+    }
+    if (cursorKeys.space.isDown)
+    {
+        if(time >= lastTime){
+            shoot();
+            lastTime = time + 100;
+        }
     }
     
     bullets.children.each(function(b) {
